c0e5732b06e9ba66c29b5dbdcfbc035d139208dc
maroze21 <praneshbalaji26@gmail.com>
2024-08-12 22:16:39 +0530

    added stuff
    bg image and login code(still working on)

---
+++ loginpage.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+
+#functions for login
+
+root =Tk()
+
+root.title("Login")
+root.geometry("800x600")
+#background
+background =PhotoImage(file="librarybg.png")
+backgroundlabel =Label(root,image=background)
+backgroundlabel.place(relwidth=1,relheight=1)
+
+#----login desgin----
+login_frame = Frame(root,bg="red",bd=5)
+login_frame.place(relx=0.5)
+
+
+
+
+
+
+root.mainloop()
